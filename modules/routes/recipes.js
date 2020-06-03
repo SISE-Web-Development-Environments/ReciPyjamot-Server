ed0c21817655ad3711bb7d550c5d57811443d3cb
HReyes <reyes@post.bgu.ac.il>
2020-06-03 19:44:11 +0300

    temp merge

--- modules/routes/recipes.js
+++ modules/routes/recipes.js
@@ -5,7 +5,48 @@ const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 //HAIM
-router.get("/recipes/recipe/{id}",async (req, res, next) => {})
+router.get("/recipes/recipe/{id}",async (req, res, next) => {
+  try{
+    // for information https://api.spoonacular.com//${id}/information
+    const infoAPI = await getRecipeInfoByID(req.query.recipe_id);//get the recipe info by given id
+    // for instrunctions GET https://api.spoonacular.com/recipes/{id}/analyzedInstructions
+    const instructionsAPI = await getRecipeInstructionsByID(req.query.recipe_id);//get the recipe intructions by given id
+    // for ingredients https://api.spoonacular.com/recipes/{id}/ingredientWidget.json
+    const ingredientsAPI = await getRecipeIngredientsByID(req.query.recipe_id)//get the recipe's ingredients by given id
+    
+    const preview = {
+      image_url: infoAPI.image,
+      title: infoAPI.title,
+      preperation_time: infoAPI.readyInMinutes,
+      likes: infoAPI.likes,
+      vegen: infoAPI.vegen,
+      gluten_free: infoAPI.gluten_freee,
+      viewed: infoAPI.viewed,//TODO get from user
+      favorite: infoAPI.favorite//TODO get from user
+    }
+    const ingrediants = ingredientsAPI.ingredients.forEach((ingredient))
+    res.send(
+      //{ data: recipe.data }
+      {
+        "preview": preview,
+        "number_of_dishes": recipe.servings,
+        "ingrediants": [
+          {
+            "name": "pepper",
+            "amount": 0,
+            "units": "grams"
+          }
+        ],
+        "instructions": {
+          "intructions": [
+            "mix the eggs with the milk"
+          ]
+        }
+      }
+      );//sends the data we got
+  }catch{}
+
+})
 //SAPIR
 router.get("/recipes",async (req, res, next) => {})
 router.post("/recipes/search",async (req, res, next) => {})
@@ -58,8 +99,27 @@ router.get("/search", async (req, res, next) => {
 });
 //#endregion
 
-function getRecipeInfo(id) {
+function getRecipeInfoByID(id) {
+// for information https://api.spoonacular.com//${id}/information
   return axios.get(`${api_domain}/${id}/information`, {
+    params: {
+      includeNutrition: false,
+      apiKey: process.env.spooncular_apiKey
+    }
+  });
+}
+function getRecipeInstructionsByID(id){
+// for instrunctions https://api.spoonacular.com/recipes/{id}/analyzedInstructions
+  return axios.get(`${api_domain}/${id}/analyzedInstructions`, {
+    params: {
+      includeNutrition: false,
+      apiKey: process.env.spooncular_apiKey
+    }
+  });
+}
+function getRecipeIngredientsByID(id){
+// for ingredients https://api.spoonacular.com/recipes/{id}/ingredientWidget.json
+  return axios.get(`${api_domain}/${id}/ingredientWidget.json`, {
     params: {
       includeNutrition: false,
       apiKey: process.env.spooncular_apiKey

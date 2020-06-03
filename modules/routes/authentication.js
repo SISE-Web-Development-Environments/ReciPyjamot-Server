f84a0b54c7303af890dafc7eb1a96f904a367d82
Sapir-Shamay <shamay.sapir@gmail.com>
2020-06-03 19:38:19 +0300

    lint fix

--- modules/routes/authentication.js
+++ modules/routes/authentication.js
@@ -1,51 +1,52 @@
-const express = require('express');
-const DButils = require('../../db/DButils');
+const express = require("express");
+const { Router } = require("express");
 
-const router = express.Router();
-const bcrypt = require('bcrypt');
+const DButils = require("../../db/DButils");
+const router = new Router();
+const bcrypt = require("bcrypt");
 
-router.post('/Register', async (req, res, next) => {
+router.post("/Register", async (req, res, next) => {
   try {
     // parameters exists
     // valid parameters
     // username exists
-    const users = await DButils.execQuery('SELECT username FROM users');
+    const users = await DButils.execQuery("SELECT username FROM users");
 
     if (users.find((x) => x.username === req.body.username)) {
-      throw {status: 409, message: 'Username taken'};
+      throw { status: 409, message: "Username taken" };
     }
 
     // add the new username
     const hash_password = bcrypt.hashSync(
-        req.body.password,
-        parseInt(process.env.bcrypt_saltRounds),
+      req.body.password,
+      parseInt(process.env.bcrypt_saltRounds)
     );
     await DButils.execQuery(
-        `INSERT INTO users VALUES (default, '${req.body.username}', '${hash_password}')`,
+      `INSERT INTO users VALUES (default, '${req.body.username}', '${hash_password}')`
     );
-    res.status(201).send({message: 'user created', success: true});
+    res.status(201).send({ message: "user created", success: true });
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/Login', async (req, res, next) => {
+router.post("/Login", async (req, res, next) => {
   try {
     // check that username exists
-    const users = await DButils.execQuery('SELECT username FROM users');
+    const users = await DButils.execQuery("SELECT username FROM users");
     if (!users.find((x) => x.username === req.body.username)) {
-      throw {status: 401, message: 'Username or Password incorrect'};
+      throw { status: 401, message: "Username or Password incorrect" };
     }
 
     // check that the password is correct
     const user = (
       await DButils.execQuery(
-          `SELECT * FROM users WHERE username = '${req.body.username}'`,
+        `SELECT * FROM users WHERE username = '${req.body.username}'`
       )
     )[0];
 
     if (!bcrypt.compareSync(req.body.password, user.password)) {
-      throw {status: 401, message: 'Username or Password incorrect'};
+      throw { status: 401, message: "Username or Password incorrect" };
     }
 
     // Set cookie
@@ -54,15 +55,15 @@ router.post('/Login', async (req, res, next) => {
     // res.cookie(session_options.cookieName, user.user_id, cookies_options);
 
     // return cookie
-    res.status(200).send({message: 'login succeeded', success: true});
+    res.status(200).send({ message: "login succeeded", success: true });
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/Logout', (req, res) => {
+router.post("/Logout", (req, res) => {
   req.session.reset(); // reset the session info --> send cookie when  req.session == undefined!!
-  res.send({success: true, message: 'logout succeeded'});
+  res.send({ success: true, message: "logout succeeded" });
 });
 
 // and finally

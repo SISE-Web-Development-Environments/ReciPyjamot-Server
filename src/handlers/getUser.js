037fbab11e21e21a8dd82c7d59cf5a82f79c632e
Sapir-Shamay <shamay.sapir@gmail.com>
2020-06-05 10:50:16 +0300

    server structure

---
+++ src/handlers/getUser.js
@@ -0,0 +1,4 @@
+const getUser = (req, res, next) => {
+  res.json({ hello: 1 });
+};
+module.exports = getUser;
---
+++ src/middleware/appMiddlewares.js
@@ -0,0 +1,7 @@
+const sessionMiddleware = require("./session");
+const appMiddleware = (app) => {
+  app.use(sessionMiddleware);
+  app.use(logger("dev")); // logger
+};
+
+module.exports = appMiddleware;

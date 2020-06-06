aae3e70ca7968a7c4d50ad789a7ced035a34bd41
HReyes <reyes@post.bgu.ac.il>
2020-06-06 18:56:53 +0300

    added login and register using the db

--- src/handlers/authLogin.js
+++ src/handlers/authLogin.js
@@ -1,5 +1,18 @@
-const authLoginHandler = (req, res, next) => {
-  res.json({ hello: 1 });
+const {login} = require("../shared");
+const authLoginHandler = async (req, res, next) => {
+  try{
+    const ans = await login(req.body.username,req.body.password);
+    res.send({
+      permission: ans,
+      massage: ans?"permission approved":"permission denied"
+    })
+  }catch(err){
+    console.log("error authenticating username and password")
+    res.send({
+      permission: false,
+      massage: "error during user authentication: "+err.message
+    })
+  }
 };
 
 module.exports = authLoginHandler;
